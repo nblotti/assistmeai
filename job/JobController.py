f77568c47340c845d59e2bfb52436b4b9033664e
nblotti <nblotti@gmail.com>
2024-10-30 15:22:29 +0100

    Serverless function

--- job/JobController.py
+++ job/JobController.py
@@ -1,6 +1,8 @@
+import os
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
+from httpx import AsyncClient
 
 from ProviderManager import job_dao_provider
 from job.Job import JobCreate, JobUpdate, JobRead
@@ -29,3 +31,17 @@ async def create(job: JobUpdate, job_repository: job_dao_provider_dep) -> JobRea
         return job_repository.update(job)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router_job.post("/request")
+async def request(job: JobCreate) -> JobRead:
+    url = os.environ["SUMMARY_SCRATCH_URL"]
+
+    async with AsyncClient(timeout=30) as client:
+        response = await client.post(url, json=job.model_dump())
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
+    response_data = response.json()
+    return JobRead(**response_data)
